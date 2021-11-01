9bc0a5a18463687cb8ec0b8e367d26d186ea16cc
Jody Brzovski <jody.brzovski@gmail.com>
2021-11-01 14:17:17 -0400

    working on first test

--- backend/test/puppy.test.js
+++ backend/test/puppy.test.js
@@ -1,4 +1,39 @@
 process.env.NODE_ENV = 'test';
 
 const chai = require('chai');
+const chaiHttp = require('chai-http');
 const expect = chai.expect;
+const should = chai.should;
+const server = require('../server')
+const puppy = require('../models/Puppy');
+
+chai.use(chaiHttp);
+
+beforeEach((done) => {
+  puppy.deleteMany({}, function(err) {});
+  done();
+});
+
+afterEach((done) => {
+  puppy.deleteMany({}, function(err) {});
+  done();
+});
+
+describe('Puppy workflow tests', () => {
+
+  it('test default API route and return all puppies', (done) => {
+
+    chai.request(server)
+    .get('/')
+    .end((err, res) => {
+      // this.enableTimeouts(false)
+      res.should.have.status(200);
+      console.log("response is: ", res);
+      res.body.should.be.a('object');    
+      done();
+    }).catch(done);
+    
+  });
+
+
+});
